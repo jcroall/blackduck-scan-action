ccd6dfc5630f4afaae0bacdcf7f89da9b2d50e82
James Croall <jcroall@pobox.com>
2021-10-31 21:59:45 +0000

    Debugging

--- blackduck-scan.py
+++ blackduck-scan.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Black Duck Security Scan")
     parser.add_argument("--url", required=True, type=str, help="Black Duck Hub URL")
     parser.add_argument("--token", required=True, type=str, help="Black Duck Hub Token")
+    parser.add_argument("--project", type=str, help="Project name")
     parser.add_argument("--mode", default="intelligent", type=str, help="Black Duck scanning mode, either intelligent or rapid")
     parser.add_argument("--output", default="blackduck-output", type=str, help="Output directory")
 
@@ -23,6 +24,7 @@ if __name__ == "__main__":
     if (url == None or token == None):
         print(f"ERROR: Must specify Black Duck Hub URL and API Token")
         sys.exit(1)
+    project = args.project
     mode = args.mode
     if (mode != "intelligent" and mode != "rapid"):
         print(f"ERROR: Scanning mode must be intelligent or rapid")
@@ -31,12 +33,18 @@ if __name__ == "__main__":
 
     detect_opts = f"--blackduck.url=\"{url}\" --blackduck.api.token=\"{token}\" --detect.blackduck.scan.mode={mode} --detect.output.path={output}"
 
+    if (project != None):
+        detect_opts = detect_opts + f" --detect.project.name {project}"
+
     print(f"INFO: Running Black Duck detect with the following options: {detect_opts}")
+    print(f"EXEC: /bin/bash {detect_cmd} {detect_opts}")
 
     result = subprocess.Popen(f"/bin/bash {detect_cmd} {detect_opts}", shell=True)
     detect_output = result.communicate()[0]
     return_code = result.returncode
 
     print(f"INFO: Done, return value {return_code}")
+
+    os.system("find . -print")
 
     sys.exit(return_code)
